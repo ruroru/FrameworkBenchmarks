97f792204a75c58a42abdc38e09467dee522ba2c
jj <keistaslogairkosulys@gmail.com>
2025-12-30 16:00:10 +0200

    test commit

--- frameworks/Clojure/kit/src/clj/io/github/kit_clj/te_bench/db/sql/hikari.clj
+++ frameworks/Clojure/kit/src/clj/io/github/kit_clj/te_bench/db/sql/hikari.clj
@@ -1,8 +1,10 @@
 (ns io.github.kit-clj.te-bench.db.sql.hikari
   (:require
+    [clojure.pprint :as pprint]
     [integrant.core :as ig]
     [kit.edge.db.sql.hikari]))
 
 (defmethod ig/prep-key :db.sql/hikari-connection
   [_ config]
+  (pprint/pprint config)
   (assoc config :maximum-pool-size 520))
