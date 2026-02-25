cadf88a40504bbbe55a70bea66f45842c1745c1c
jj <keistaslogairkosulys@gmail.com>
2026-02-25 22:46:23 +0100

    Test boasql with vertx backend

--- frameworks/Clojure/ring-http-exchange/src/ring_http_exchange/string_handler.clj
+++ frameworks/Clojure/ring-http-exchange/src/ring_http_exchange/string_handler.clj
@@ -2,7 +2,7 @@
   (:require
     [jj.majavat :as majavat]
     [ring-http-exchange.model :as model])
-  (:import (java.util.function Consumer)))
+  )
 
 (defrecord Response [body status headers])
 (def ^:private ^:const fortune-headers {"Server"       "ring-http-exchange"
@@ -14,13 +14,14 @@
 
 (def ^:private render-fortune (majavat/build-html-renderer "fortune.html"))
 
-(deftype FortuneCallback [respond]
-  Consumer
-  (accept [_ fortune-data]
+(defn create-callback [respond]
+  (fn [fortune-data]
     (respond
       (Response.
-        (render-fortune {:messages (sort-by :message (conj fortune-data {:id      0
-                                                                         :message "Additional fortune added at request time."}))})
+        (render-fortune {:messages
+                         (sort-by :message
+                                  (conj fortune-data {:id      0
+                                                      :message "Additional fortune added at request time."}))})
         200
         fortune-headers))))
 
@@ -36,5 +37,11 @@
 (defn get-async-handler [data-source]
   (fn [req respond raise]
     (if (.equals "/fortunes" (req :uri))
-      (model/async-query-fortunes data-source (FortuneCallback. respond) raise)
+      (model/async-query-fortunes data-source (create-callback respond) raise)
+      (Response. model/hello-world 200 plain-text-headers))))
+
+(defn get-vertx-handler [data-source]
+  (fn [req respond raise]
+    (if (.equals "/fortunes" (req :uri))
+      (model/vertx-query-fortunes data-source (create-callback respond) raise)
       (Response. model/hello-world 200 plain-text-headers))))
